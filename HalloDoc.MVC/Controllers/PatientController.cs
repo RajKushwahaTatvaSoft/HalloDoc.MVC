6dbf836c58757e86ecfb92f9a7c869e72be91f42
PCA195 <PCA195@PCA195>
2024-02-02 18:13:05 +0530

    Add project files.

---
+++ HalloDoc.MVC/Controllers/PatientController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDoc.MVC.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ForgetPassword(){
+            return View();
+        }
+
+        public IActionResult SubmitRequest()
+        {
+            return View();
+        }
+    }
+}
