7d7397599824ff338a83fb92c0529f70282df146
RajKushwahaTatvaSoft <raj.kushwaha@etatvasoft.com>
2024-02-06 17:34:01 +0530

    Integrated Postgresql Database

--- HalloDoc.MVC/wwwroot/js/site.js
+++ HalloDoc.MVC/wwwroot/js/site.js
@@ -1,20 +1,24 @@
-﻿var isLightMode = true
+﻿localStorage.setItem("isDark", false);
 
-localStorage.setItem("isLightMode", true)
+window.onload = function () {
 
-window.onload =
-    function () {
-        if (localStorage.getItem('isLightMode') === 'false') {
-            setDarkMode();
-        }
+    Boolean isDark = localStorage.getItem("isDark");
+    if (isDark) {
+        document.body.classList.add("dark-mode");
     }
-
-function myFunction() {
-    localStorage.setItem("isLightMode", !isLightMode)
-    location.reload();
 }
 
-function setDarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+function togglePassword() {
+    var text = document.getElementById("login-pass-text");
+    var btn = document.getElementById("login-pass-btn");
+
+    if (text.type == "text") {
+        text.type = "password";
+        btn.src = "/images/password_not_visible.svg";
+    }
+    else {
+        text.type = "text";
+        btn.src = "/images/password_icon.svg";
+    }
+
 }
